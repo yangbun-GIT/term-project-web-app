1ffc4ad73bf5c2b938f060667914b28110aa25e1
yangbun-GIT <minurel5658@gmail.com>
2025-12-09 00:03:09 +0900

    배포 자동화 설정 및 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    // [중요] GitHub Pages 배포를 위한 기본 경로 설정
+    // 리포지토리 이름과 동일하게 맞춰야 합니다.
+    base: '/WSD-Assignment-02/',
 })
